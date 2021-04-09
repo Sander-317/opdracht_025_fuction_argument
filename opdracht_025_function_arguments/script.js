30185da89142da58f07a3d4add33d575a2becd9e
Sander-317 <80258328+Sander-317@users.noreply.github.com>
2021-04-09 14:26:45 +0200

    Add files via upload

---
+++ opdracht_025_function_arguments/script.js
@@ -0,0 +1,10 @@
+
+
+
+const wallPaint = function (color, wallSide) {
+    console.log("the " + wallSide + " wall has been painted " + color);
+};
+
+wallPaint("north", "green");
+wallPaint("west", "blue");
+wallPaint();
